069baae1001666171b79424cc32d289e0e0aedff
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2023-05-08 21:43:01 +1200

    Setup initial project

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main extends GameEngine {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        createGame(new Main(), 30);
+    }
+
+    @Override
+    public void update(double dt) {
+
+    }
+
+    @Override
+    public void paintComponent() {
+
+    }
+}
