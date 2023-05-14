5e2bf22687eca16a1f800e79176442d5f249e985
owlbeach <reecebonnington1@gmail.com>
2023-05-14 17:08:54 +1200

    Main Menu
    The two buttons for play and quit are now clickable. Play currently does nothing as we need code to star the game. Quit closes the game window.

--- src/nz/ac/massey/games_programming/Main.java
+++ src/nz/ac/massey/games_programming/Main.java
@@ -6,6 +6,7 @@
 
 package nz.ac.massey.games_programming;
 
+import java.awt.event.MouseEvent;
 import java.awt.*; // For Color
 
 public class Main extends GameEngine {
@@ -39,5 +40,39 @@ public class Main extends GameEngine {
         changeColor(Color.black);
         drawCenteredText(250, "Play", "Arial", 65);
         drawCenteredText(400, "Quit", "Arial", 65);
+    }
+
+    // Manages user mouse button input
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // Gets the x and y co-ordinates where the user clicked
+        int x = e.getX();
+        int y = e.getY();
+
+        // Return if non-left mouse click
+        if (e.getButton() != MouseEvent.BUTTON1) {
+            return;
+        }
+
+        // If user clicks the Play button, start the game
+        if (x > 570 && y > 190) {
+            if (x < 716 && y < 270) {
+                System.out.println("Starting the game!");
+                // TO-DO: Add code that starts the game here
+            }
+        }
+
+        // If user clicks the Quit button, exit the game
+        if (x > 570 && y > 340) {
+            if (x < 710 && y < 410) {
+                System.out.println("Exiting game...");
+                System.exit(420);
+                mFrame.dispose();
+                mFrame.setVisible(false);
+            }
+        }
+
+        // If the user left-clicks the screen, display that position
+        System.out.println("Left click at position (" + x + ", " + y + ")");
     }
 }
