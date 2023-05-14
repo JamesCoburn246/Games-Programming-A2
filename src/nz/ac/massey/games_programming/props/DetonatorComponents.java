0cc43ce2cde5bbee0024ab20619caa3bc397437a
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2023-05-15 02:09:14 +1200

    Add collectables

---
+++ src/nz/ac/massey/games_programming/props/DetonatorComponents.java
@@ -0,0 +1,9 @@
+package nz.ac.massey.games_programming.props;
+
+import java.awt.*;
+
+public class DetonatorComponents extends Collectable {
+    public DetonatorComponents(PropType type, int x, int y, Image sprite) {
+        super(type, x, y, sprite);
+    }
+}
---
+++ src/nz/ac/massey/games_programming/props/Gear.java
@@ -0,0 +1,9 @@
+package nz.ac.massey.games_programming.props;
+
+import java.awt.*;
+
+public class Gear extends Collectable {
+    public Gear(PropType type, int x, int y, Image sprite) {
+        super(type, x, y, sprite);
+    }
+}
