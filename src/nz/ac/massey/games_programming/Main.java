6b761307d1291b30cee3b647c9ea6d92859030a1
TurriaN <turrian.steam@gmail.com>
2023-05-16 11:25:57 +1200

    Added Basic player Controls
    Basic KeyPressed Events in main for all keys listed in the player controls issue.
    
    None are bind for now.

--- src/nz/ac/massey/games_programming/Main.java
+++ src/nz/ac/massey/games_programming/Main.java
@@ -6,7 +6,10 @@
 
 package nz.ac.massey.games_programming;
 
-public class Main extends GameEngine {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Main extends GameEngine implements KeyListener {
 
     private int winWidth = 1280, winHeight = 720;
 
@@ -28,5 +31,58 @@ public class Main extends GameEngine {
     @Override
     public void paintComponent() {
 
+    }
+
+
+    public void keyPressed(KeyEvent e) {
+        ///////// |W A S D - Movement Keys /////////////
+        if(e.getKeyCode() == KeyEvent.VK_W){
+            System.out.println("W Key is Pressed");
+
+        }
+        if(e.getKeyCode() == KeyEvent.VK_A){
+            System.out.println("A Key is Pressed");
+
+        }
+        if(e.getKeyCode() == KeyEvent.VK_S){
+            System.out.println("S Key is Pressed");
+
+        }
+        if(e.getKeyCode() == KeyEvent.VK_D){
+            System.out.println("D Key is Pressed");
+
+        }
+        if(e.getKeyCode() == KeyEvent.VK_UP){
+            System.out.println("UP Arrow Key is Pressed");
+
+        }
+
+        ///////// |Arrow Key - Movement Keys /////////////
+        if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            System.out.println("Left Arrow Key is Pressed");
+
+        }
+        if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            System.out.println("Down Arrow Key is Pressed");
+
+        }
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            System.out.println("Right Arrow Key is Pressed");
+
+        }
+
+        ///////// |Special Keys - Interaction and Pause Menu /////////////
+        if(e.getKeyCode() == KeyEvent.VK_SPACE){
+            System.out.println("Space Bar Key is Pressed");
+
+        }
+        if(e.getKeyCode() == KeyEvent.VK_E){
+            System.out.println("E Key is Pressed");
+
+        }
+        if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+            System.out.println("ESC Key is Pressed");
+
+        }
     }
 }
