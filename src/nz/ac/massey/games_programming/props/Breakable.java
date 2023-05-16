64912f481226e5b0d538045b2ab4a9aeb052b0ee
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2023-05-16 21:59:03 +1200

    Add dealDamage function

--- src/nz/ac/massey/games_programming/props/Breakable.java
+++ src/nz/ac/massey/games_programming/props/Breakable.java
@@ -17,5 +17,20 @@ public class Breakable extends SpriteProp {
 
     public void setHealth(int health) {
         this.health = health;
+
+    /**
+     * Deal damage to this sprite; potentially destroying it in the process.
+     * @param damage the amount of damage to be done to health.
+     * @return true if the object was destroyed, or false if it survived.
+     */
+    public boolean dealDamage(int damage) {
+        setHealth(getHealth() - damage);
+        if (getHealth() <= 0) {
+            setHealth(0);
+
+            return true;
+        } else {
+            return false;
+        }
     }
 }
