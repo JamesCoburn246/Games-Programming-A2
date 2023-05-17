5a4f28eabcf8dc8824ff9692f733e30a7fad5a8b
TurriaN <turrian.steam@gmail.com>
2023-05-17 14:37:59 +1200

    Basic Grid Logic
    Added Basic Logic for Grid. Does not restrict movement of player. Able to pinpoint a grid reference from a given point which eventually should be fed the current player location.
    
    Added isGameStarted Bool
    Added an example of the use of grid and bound to spacebar from dropping bombs

--- src/nz/ac/massey/games_programming/Main.java
+++ src/nz/ac/massey/games_programming/Main.java
@@ -11,24 +11,52 @@ import java.awt.event.MouseEvent;
 import java.awt.*; // For Color
 
 public class Main extends GameEngine {
+
     private static final Color lightBlue = new Color(65, 77, 100);
-    private int winWidth = 1280, winHeight = 720;
+    public int winWidth = 1280, winHeight = 720;
+
+
 
     public static void main(String[] args) {
+
         createGame(new Main(), 30);
+
+
     }
 
     public void init() {
         setWindowSize(winWidth, winHeight);
+
+
     }
+
+
+
+
+
 
     @Override
     public void update(double dt) {
+
+        if (bombDropped == true && isgameStarted == true) {          // Currently set to Space Bar (Maybe Space to place, "E" to blow up bombs?
+            Grid grid = new Grid();
+            int winWidth = 1280;
+            int winHeight = 720;
+            int pointX = 500;  // Example,Change to currentPlayPos or whatever
+            int pointY = 500;  // Example, currentPlayPos or whatever
+            int cellIndex = grid.calculateGrid(winWidth, winHeight, pointX, pointY);
+            System.out.println("Bomb Dropped At Grid Refence: " + cellIndex);
+            bombDropped = false;
+
+        }
+
+
 
     }
 
     @Override
     public void paintComponent() {
+
         displayMainMenu();
     }
 
@@ -44,6 +72,8 @@ public class Main extends GameEngine {
     }
 
     // Manages user keyboard input
+    int keyPressed;
+    boolean bombDropped;
     @Override
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
@@ -55,26 +85,32 @@ public class Main extends GameEngine {
             // If user presses W or up arrow
             case KeyEvent.VK_W, KeyEvent.VK_UP -> {
                 System.out.println("KeyPressed: Up");
+                keyPressed = 1;
             }
             // If user presses A or left arrow
             case KeyEvent.VK_A, KeyEvent.VK_LEFT -> {
                 System.out.println("KeyPressed: Left");
+                keyPressed = 2;
             }
             // If user presses S or down arrow
             case KeyEvent.VK_S, KeyEvent.VK_DOWN -> {
                 System.out.println("KeyPressed: Down");
+                keyPressed = 3;
             }
             // If user presses D or right arrow
             case KeyEvent.VK_D, KeyEvent.VK_RIGHT -> {
                 System.out.println("KeyPressed: Right");
+                keyPressed = 4;
             }
 
             ///////// Special Keys - Interaction and Pause Menu /////////////
             // If user presses space
             case KeyEvent.VK_SPACE -> {
                 System.out.println("KeyPressed: Space");
+                bombDropped = true;
+
             }
-            // If user presses E
+            // If user presses E //// DROP BOMB
             case KeyEvent.VK_E -> {
                 System.out.println("KeyPressed: E");
             }
@@ -86,7 +122,7 @@ public class Main extends GameEngine {
 
         }
     }
-
+    boolean isgameStarted;
     // Manages user mouse button input
     @Override
     public void mousePressed(MouseEvent e) {
@@ -103,6 +139,7 @@ public class Main extends GameEngine {
         if (x > 570 && y > 190) {
             if (x < 716 && y < 270) {
                 System.out.println("Starting the game!");
+                isgameStarted = true;
                 // TO-DO: Add code that starts the game here
             }
         }
@@ -111,6 +148,7 @@ public class Main extends GameEngine {
         if (x > 570 && y > 340) {
             if (x < 710 && y < 410) {
                 System.out.println("Exiting game...");
+                isgameStarted = false;
                 System.exit(420);
                 mFrame.dispose();
                 mFrame.setVisible(false);
@@ -120,4 +158,38 @@ public class Main extends GameEngine {
         // If the user left-clicks the screen, display that position
         // System.out.println("Left click at position (" + x + ", " + y + ")"); // Use for debugging
     }
+
+    /////////// GRID LOGIC ////////////// Works out cells of 33x33 based off the given resolution (1280x720) and calculates a grid reference.
+    public class Grid {
+
+        public int calculateGrid(int winWidth, int winHeight, int pointX, int pointY) {
+            int cellWidth = 33;     // Matching 33x33 Sprites
+            int cellHeight = 33;    // Matching 33x33 Sprites
+
+            int numRows = winHeight / cellHeight;
+            int numCols = winWidth / cellWidth;
+
+            int cellIndex = -1;  // Default value if Given point is outside boundary
+
+            for (int row = 0; row < numRows; row++) {
+                for (int col = 0; col < numCols; col++) {
+                    int x = col * cellWidth;
+                    int y = row * cellHeight;
+
+                    // Check if the point is within the current cell
+                    if (pointX >= x && pointX < x + cellWidth && pointY >= y && pointY < y + cellHeight) {
+                        cellIndex = row * numCols + col;
+                        break;
+                    }
+                }
+                if (cellIndex != -1) {
+                    break;
+                }
+            }
+
+            return cellIndex;
+        }
+
+    }
+
 }
