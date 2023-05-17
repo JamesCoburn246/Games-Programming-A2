d0ed29d2f7351a08cfc59c229dd9c6b61a56de29
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2023-05-17 21:50:15 +1200

    Add references to container

--- src/nz/ac/massey/games_programming/props/Loot.java
+++ src/nz/ac/massey/games_programming/props/Loot.java
@@ -1,9 +1,10 @@
 package nz.ac.massey.games_programming.props;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Loot extends Collectable {
-    public Loot(PropType type, int x, int y, Image sprite) {
-        super(type, x, y, sprite);
+    public Loot(PropType type, int x, int y, Image sprite, ArrayList<SpriteProp> container) {
+        super(type, x, y, sprite, container);
     }
 }
