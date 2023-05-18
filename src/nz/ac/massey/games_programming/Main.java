8917fb020a7b6ea068e5378529b6e81dd30d0dd6
TurriaN <turrian.steam@gmail.com>
2023-05-18 15:13:24 +1200

    Moved GameState to new Class File (Broken)
    James will take a look.

--- src/nz/ac/massey/games_programming/Main.java
+++ src/nz/ac/massey/games_programming/Main.java
@@ -187,43 +187,5 @@ public class Main extends GameEngine {
         // System.out.println("Left click at position (" + x + ", " + y + ")"); // Use for debugging
     }
 
-    public class GameState {
-        private State currentState;
 
-        // Game State Getter
-        public State getGameState() {
-            return currentState;
-        }
-
-        // GameState Setter - When the GameState changes the Audio changes along with it
-        public void setGameState(State state) {
-            currentState = state;
-            AudioClip MenuMusic = loadAudio("Sounds/MenuMusic.wav");
-            AudioClip GameMusic = loadAudio("Sounds/MenuMusic.wav");
-            AudioClip GameStart = loadAudio("Sounds/gameStart.wav");
-            if (currentState == State.MAIN_MENU) {
-                stopAudioLoop(GameMusic);
-                startAudioLoop(MenuMusic, 20);
-            }
-            if (currentState == State.PLAYING) {
-                stopAudioLoop(MenuMusic);
-                playAudio(GameStart);
-                startAudioLoop(GameMusic, 20);
-            }
-            if (currentState == State.PAUSED) {
-                stopAudioLoop(GameMusic);
-                startAudioLoop(MenuMusic, 20);
-            }
-        }
-
-        public boolean is(State state) {
-            return (this.currentState == state);
-        }
-
-        // Available Game States
-        public enum State {
-            MAIN_MENU, PLAYING, PAUSED, GAME_OVER
-        }
-
-    }
 }
