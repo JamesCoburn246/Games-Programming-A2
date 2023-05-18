b5c3582903e767c2f5d0c83ea9fb55c52849ddde
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2023-05-18 14:56:59 +1200

    Add fuse countdown logic

--- src/nz/ac/massey/games_programming/props/Explosive.java
+++ src/nz/ac/massey/games_programming/props/Explosive.java
@@ -7,31 +7,62 @@ import java.awt.*;
 
 public class Explosive extends SpriteProp {
 
+    private static Image[] fuseAnimation;
+    private static Image[] explosionAnimation;
     private GameEngine engine;
-    private int fuse;
+    private int initialFuse;
+    private int fuseRemaining;
     private int damage;
     private int range;
-    private boolean ready = false;
+    private ExplosiveState state = ExplosiveState.IDLE;
+
+    static {
+        // TODO Load animations.
+        fuseAnimation = null;
+        explosionAnimation = null;
+    }
 
     public Explosive(int x, int y, Grid.Cell cell) {
         super(PropType.EXPLOSIVE, x, y, cell);
+        this.durationPerFrame = 0.35;
+        this.setSprites(fuseAnimation);
     }
 
-    public Explosive(int x, int y, Grid.Cell cell, GameEngine engine, int damage, int range) {
-        super(PropType.EXPLOSIVE, x, y, cell);
-        this.engine = engine;
+    @Override
+    public void update(double dt) {
+        // Prevent animations from playing while the explosive is idle.
+        if (state != ExplosiveState.IDLE) {
+            super.update(dt);
+        }
+    }
+
+    public void lightFuse(int fuse, int damage, int range) {
+        this.initialFuse = fuse;
         this.damage = damage;
         this.range = range;
-    }
-
-    public void lightFuse(int fuse) {
-        this.fuse = fuse;
-        System.out.println("The fuse has been lit.");
+        this.state = ExplosiveState.FUSE;
     }
 
     @Override
     public void outOfFrames() {
-        super.cell.clearContents();
-        // TODO Detonate explosive?
+        switch (state) {
+            // This should not happen.
+            case IDLE -> {
+                System.out.println("An error has occurred! An idle explosive should never run out of frames!");
+                System.exit(1);
+            }
+            // Change to the explosion animation.
+            case FUSE -> {
+                this.setSprites(explosionAnimation);
+                this.spriteIndex = 0;
+                this.durationPerFrame = 0.35;
+            }
+            // Remove reference to self, effectively self-destruct.
+            case EXPLODE -> super.cell.clearContents();
+        }
+    }
+
+    private enum ExplosiveState {
+        IDLE, FUSE, EXPLODE
     }
 }
