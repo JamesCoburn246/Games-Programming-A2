e731fd0d6b51db0a23f311db63a4dddd2e50759a
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2023-05-19 16:11:53 +1200

    Add rock sprite

--- src/nz/ac/massey/games_programming/props/Stone.java
+++ src/nz/ac/massey/games_programming/props/Stone.java
@@ -1,5 +1,6 @@
 package nz.ac.massey.games_programming.props;
 
+import nz.ac.massey.games_programming.GameEngine;
 import nz.ac.massey.games_programming.Grid;
 
 import java.awt.*;
@@ -7,9 +8,15 @@ import java.awt.*;
 public class Stone extends Breakable {
 
     private static final int INITIAL_HEALTH = 3;
+    private static final Image ROCK;
+
+    static {
+        Image sprites = GameEngine.loadImage("Images/Objects/RockExplosion.png");
+        ROCK = GameEngine.subImage(sprites, 0, 0, 32, 32);
+    }
 
     public Stone(int x, int y, Grid.Cell cell) {
         super(PropType.ROCK, x, y, cell, INITIAL_HEALTH);
+        this.setSprite(ROCK);
     }
-
 }
