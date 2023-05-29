680a035a57ab3b2400954cc7caf2c98d78de9722
owlbeach <reecebonnington1@gmail.com>
2023-05-29 17:01:17 +1200

    Added player to bomb crate class

--- src/nz/ac/massey/games_programming/props/BombCrate.java
+++ src/nz/ac/massey/games_programming/props/BombCrate.java
@@ -7,19 +7,22 @@ import java.awt.*;
 
 public class BombCrate extends Collectable {
     private static final Image BOMBCRATE;
+    private final Player player;
 
     static {
         Image bombSprite = GameEngine.loadImage("Images/Objects/BombCrate.png");
         BOMBCRATE = GameEngine.subImage(bombSprite, 0, 0, 32, 32);
     }
-    public BombCrate(PropType type, int x, int y, Grid.Cell cell) {
+    public BombCrate(PropType type, int x, int y, Grid.Cell cell, Player player) {
         super(type, x, y, cell);
         this.setSprite(BOMBCRATE);
+        this.player = player;
     }
 
     // TO-DO: This needs to be overridden once the abstract onCollection is added
     public void onCollection() {
         // Add a bomb to the explosive count
+        player.addExplosive();
 
         // Play the sound of collecting the bomb crate
     }
