453b4d33d160b899f4ff20a69e11e141251fff66
owlbeach <reecebonnington1@gmail.com>
2023-06-08 00:20:19 +1200

    Added rock explosion image

--- src/nz/ac/massey/games_programming/props/Rock.java
+++ src/nz/ac/massey/games_programming/props/Rock.java
@@ -10,7 +10,8 @@ public class Rock extends Breakable {
     private static final float ANIMATION_DURATION = 0.33F;
     private static final int FRAME_WIDTH = 32, FRAME_HEIGHT = 32;
     private static final int FRAME_COUNT = 3;
-    private static final Image[] ROCK = new Image[FRAME_COUNT];
+    private static final Image[] ROCK_CRACKING = new Image[FRAME_COUNT];
+    private static final Image[] ROCK_EXPLOSION = new Image[FRAME_COUNT];
 
     private int frameCounter = 0;
 
@@ -18,13 +19,14 @@ public class Rock extends Breakable {
         Image rockBreakingSprite = GameEngine.loadImage("Images/Objects/RockBreaking.png");
         Image rockExplosionAnimaton = GameEngine.loadImage("Images/Objects/RockExplosion.png");
         for (int i = 0; i < FRAME_COUNT; i++) {
-            ROCK[i] = GameEngine.subImage(rockBreakingSprite, FRAME_WIDTH * i, 0, FRAME_WIDTH, FRAME_HEIGHT);
+            ROCK_CRACKING[i] = GameEngine.subImage(rockBreakingSprite, FRAME_WIDTH * i, 0, FRAME_WIDTH, FRAME_HEIGHT);
+            ROCK_EXPLOSION[i] = GameEngine.subImage(rockExplosionAnimaton, FRAME_WIDTH * i, 0, FRAME_WIDTH, FRAME_HEIGHT);
         }
     }
 
     public Rock(int x, int y, Grid.Cell cell) {
         super(PropType.ROCK, x, y, cell, FRAME_COUNT);
-        this.setSprite(ROCK[0]);
+        this.setSprite(ROCK_CRACKING[0]);
         this.durationPerFrame = ANIMATION_DURATION / FRAME_COUNT;
     }
 
@@ -34,7 +36,7 @@ public class Rock extends Breakable {
         if (!isDestroyed) {
             frameCounter++;
             if (frameCounter < FRAME_COUNT) {
-                this.setSprite(ROCK[frameCounter]);
+                this.setSprite(ROCK_CRACKING[frameCounter]);
             }
         }
         else {
