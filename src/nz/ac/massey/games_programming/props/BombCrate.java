a520874a3071a4ec9a1a7eb9cb965938a5fb683c
owlbeach <reecebonnington1@gmail.com>
2023-06-08 12:56:04 +1200

    Add audio to bomb crate collection

--- src/nz/ac/massey/games_programming/props/BombCrate.java
+++ src/nz/ac/massey/games_programming/props/BombCrate.java
@@ -2,16 +2,20 @@ package nz.ac.massey.games_programming.props;
 
 import nz.ac.massey.games_programming.GameEngine;
 import nz.ac.massey.games_programming.Grid;
+import nz.ac.massey.games_programming.Main;
 
 import java.awt.*;
 
 public class BombCrate extends Collectable {
     private static final Image BOMBCRATE;
+    private static final GameEngine.AudioClip pickup;
     private final Player player;
+    private Main game;
 
     static {
         Image bombSprite = GameEngine.loadImage("Images/Objects/BombCrate.png");
         BOMBCRATE = GameEngine.subImage(bombSprite, 0, 0, 32, 32);
+        pickup = GameEngine.loadAudio("PickUp.wav");
     }
     public BombCrate(PropType type, int x, int y, Grid.Cell cell) {
         super(type, x, y, cell);
@@ -25,5 +29,6 @@ public class BombCrate extends Collectable {
         player.addExplosive();
 
         // Play the sound of collecting the bomb crate
+        game.startAudioLoop(pickup, 10);
     }
 }
